1ce5dd1df2e55489954f457e8778463a6e637ed0
grantkee <grant.d.kee@gmail.com>
2020-04-18 14:33:54 -0500

    controller using INNERJOIN to return question and answer options.

--- server/controllers/quiz.js
+++ server/controllers/quiz.js
@@ -3,6 +3,15 @@ const pool = require('../sql/connection');
 const {handleSQLError} = require('../sql/error');
 
 const getQuestion = (req, res) => {
-  let sql = 'SELECT * FROM questions WHERE id = ?'
+  let sql = 'SELECT q.question, a.answer_type, a.content FROM answers a INNER JOIN questions q ON q.id = a.question_id WHERE q.id = ?'
   sql = mysql.format(sql, [req.params.id]);
-}
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+module.exports = {
+  getQuestion
+};
