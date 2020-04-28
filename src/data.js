4548071d0b54e72ca4496dfe03502c07c010efc9
grantkee <grant.d.kee@gmail.com>
2020-04-28 10:21:32 -0500

    dummy data created, but database is back up and runnning

--- src/data.js
+++ src/data.js
@@ -3,28 +3,28 @@ export const questionsArray = [
   { question_id: 2, question: "How do you view your coworkers and boss?"},
   { question_id: 3, question: "If you were to quit a job: how do you say goodbye?"},
   { question_id: 4, question: "In a spouse: I look for someone who...."},
-  { question_id: 5, question: "Your thoughts on cheating?"},
+  { question_id: 5, question: "Your thoughts on cheating?"}
 ];
 
 export const answersArray = [
-  {question_id: 1, type: Paul, George, Ringo, John
-  {question_id: 2, type: Paul, George, Ringo, John
-  {question_id: 3, type: Paul, George, Ringo, John
-  {question_id: 4, type: Paul, George, Ringo, John
-  {question_id: 5, type: Paul, George, Ringo, John
-  {question_id: 1, type: Paul, George, Ringo, John
-  {question_id: 2, type: Paul, George, Ringo, John
-  {question_id: 3, type: Paul, George, Ringo, John
-  {question_id: 4, type: Paul, George, Ringo, John
-  {question_id: 5, type: Paul, George, Ringo, John
-  {question_id: 1, type: Paul, George, Ringo, John
-  {question_id: 2, type: Paul, George, Ringo, John
-  {question_id: 3, type: Paul, George, Ringo, John
-  {question_id: 4, type: Paul, George, Ringo, John
-  {question_id: 5, type: Paul, George, Ringo, John
-  {question_id: 1, type: Paul, George, Ringo, John
-  {question_id: 2, type: Paul, George, Ringo, John
-  {question_id: 3, type: Paul, George, Ringo, John
-  {question_id: 4, type: Paul, George, Ringo, John
-  {question_id: 5, type: Paul, George, Ringo, John
+  {question_id: 1, type: Paul, content: "Important if I love what I'm doing. If not; I could care less" },
+  {question_id: 2, type: Paul, content: "They are all just people I work with. I like some more than others; but eh. What are you gonna do?" },
+  {question_id: 3, type: Paul, content: "Just leave; but dream about burning the place to the ground" },
+  {question_id: 4, type: Paul, content: "Adores me like a God" },
+  {question_id: 5, type: Paul, content: "I try to keep things simple and drama-free; so I'm not crazy about it" },
+  {question_id: 1, type: George, content: "Everything; regardless of what I'm doing. I play to win" },
+  {question_id: 2, type: George, content: "I'm not crazy about bossy bosses or too many coworkers" },
+  {question_id: 3, type: George, content: "Make sure all harsh feelings are patched up and make sure everyone knows I've enjoyed them" },
+  {question_id: 4, type: George, content: "Is truly my equal" },
+  {question_id: 5, type: George, content: "No way. Think about how I will be portrayed if I'm caught!" },
+  {question_id: 1, type: Ringo, content: "Somewhat important. I know when I've succeeded; but I also know when I've failed" },
+  {question_id: 2, type: Ringo, content: "Bosses I must please; coworkers must please me" },
+  {question_id: 3, type: Ringo, content: "Tell everyone EXACTLY how I feel about them; including my boss; trash the place; and get escorted off the premisis" },
+  {question_id: 4, type: Ringo, content: "Is loving; intelligent and easygoing" },
+  {question_id: 5, type: Ringo, content: "Only if I'm really not in love" },
+  {question_id: 1, type: John, content: "Not important. I just want a check; man" },
+  {question_id: 2, type: John, content: "Bosses are only trying to bring me down; and coworkers either understand me or are complete and total idiots" },
+  {question_id: 3, type: John, content: "Send around a thank you e-mail; even if I could care less what would happen to those blokes" },
+  {question_id: 4, type: John, content: "Wants nothing but fun; love; and sex" },
+  {question_id: 5, type: John, content: "If it feels good; do it" }
 ]
